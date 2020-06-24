d987180421279fe459ba2c24f238ef21e4dbc7c9
fishwickid <chadfishwick@gmail.com>
2020-06-25 07:30:55 +0800

    added user form code

--- script03.js
+++ script03.js
@@ -61,6 +61,55 @@ function renderQuestion() {
 };
 
 
+//User form
+
+
+// // form.js
+// const formId = "save-later-form"; // ID of the form
+// const url = location.href; //  href for the page
+// const formIdentifier = `${url} ${formId}`; // Identifier used to identify the form
+// const saveButton = document.querySelector("#save"); // select save button
+// const alertBox = document.querySelector(".alert"); // select alert display div
+// let form = document.querySelector(`#${formId}`); // select form
+// let formElements = form.elements; // get the elements in the form
+// /**
+//  * This function gets the values in the form
+//  * and returns them as an object with the
+//  * [formIdentifier] as the object key
+//  * @returns {Object}
+//  */
+// const getFormData = () => {
+//   let data = { [formIdentifier]: {} };
+//   for (const element of formElements) {
+//     if (element.name.length > 0) {
+//       data[formIdentifier][element.name] = element.value;
+//     }
+//   }
+//   return data;
+// };
+// saveButton.onclick = event => {
+//   event.preventDefault();
+//   data = getFormData();
+//   localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+//   const message = "Form draft has been saved!";
+//   displayAlert(message);
+// };
+// /**
+//  * This function populates the form
+//  * with data from localStorage
+//  *
+//  */
+// const populateForm = () => {
+//   if (localStorage.key(formIdentifier)) {
+//     const savedData = JSON.parse(localStorage.getItem(formIdentifier)); // get and parse the saved data from localStorage
+//     for (const element of formElements) {
+//       if (element.name in savedData) {
+//         element.value = savedData[element.name];
+//       }
+//     }
+//   }
+// };
+
 
 
   // Storing our giphy API URL for a random cat image
